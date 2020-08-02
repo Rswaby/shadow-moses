c3e7f98453714386faa2e403b0bc749a6751be97
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-08-02 19:51:26 -0400

    graphql context

--- api/src/main/java/com/shadowmoses/api/model/Auth/AuthContext.java
+++ api/src/main/java/com/shadowmoses/api/model/Auth/AuthContext.java
@@ -2,21 +2,31 @@ package com.shadowmoses.api.model.Auth;
 
 import com.shadowmoses.api.model.User;
 import graphql.servlet.GraphQLContext;
+import graphql.servlet.GraphQLContextBuilder;
+import graphql.servlet.SimpleGraphQLHttpServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.HandshakeRequest;
 import java.util.Optional;
 
-//@Component
-public class AuthContext extends GraphQLContext {
-//    @Autowired
-    private final User user;
-
-    public AuthContext (User user, Optional<HttpServletRequest> request, Optional<HttpServletResponse> response){
-//        super(request);
-        this.user = user;
-
+@Component
+public class AuthContext implements GraphQLContextBuilder {
+    @Override
+    public GraphQLContext build(HttpServletRequest httpServletRequest) {
+        return null;
     }
+
+    @Override
+    public GraphQLContext build(HandshakeRequest handshakeRequest) {
+        return null;
+    }
+
+    @Override
+    public GraphQLContext build() {
+        return null;
+    }
+    
 }
--- api/src/test/java/com/shadowmoses/api/ApiApplicationTests.java
+++ api/src/test/java/com/shadowmoses/api/ApiApplicationTests.java
@@ -2,7 +2,9 @@ package com.shadowmoses.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+//@ContextConfiguration(classes = {ApiApplication.class})
 @SpringBootTest
 class ApiApplicationTests {
 
