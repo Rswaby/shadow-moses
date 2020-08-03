ada8f910ad65a90f147b8f4bdd7c3033ef94d6ca
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-08-02 21:30:58 -0400

    clean up

--- api/src/main/java/com/shadowmoses/api/model/Auth/AuthContext.java
+++ api/src/main/java/com/shadowmoses/api/model/Auth/AuthContext.java
@@ -1,32 +1,12 @@
 package com.shadowmoses.api.model.Auth;
 
-import com.shadowmoses.api.model.User;
-import graphql.servlet.GraphQLContext;
-import graphql.servlet.GraphQLContextBuilder;
-import graphql.servlet.SimpleGraphQLHttpServlet;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.HandshakeRequest;
-import java.util.Optional;
+
 
 @Component
-public class AuthContext implements GraphQLContextBuilder {
-    @Override
-    public GraphQLContext build(HttpServletRequest httpServletRequest) {
-        return null;
-    }
+public class AuthContext  {
+    public static final String BEARER_PREFIX = "Bearer ";
 
-    @Override
-    public GraphQLContext build(HandshakeRequest handshakeRequest) {
-        return null;
-    }
-
-    @Override
-    public GraphQLContext build() {
-        return null;
-    }
-    
 }
--- api/src/test/java/com/shadowmoses/api/ApiApplicationTests.java
+++ api/src/test/java/com/shadowmoses/api/ApiApplicationTests.java
@@ -2,9 +2,7 @@ package com.shadowmoses.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
-//@ContextConfiguration(classes = {ApiApplication.class})
 @SpringBootTest
 class ApiApplicationTests {
 
